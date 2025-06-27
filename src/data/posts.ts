e82076eb1967958612199c9cda0987175b962d72
daganspano <daganspano@gmail.com>
2025-06-27 14:52:40 -0500

    Add content type preferences to register and account data

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -13,6 +13,6 @@ export const posts: Post[] = [
     title: "Test Post 1",
     content:
       "This is the content of test post 1. It contains some sample text to demonstrate the post structure.",
-    contentTypes: ["test", "sample", "demo"],
+    contentTypes: ["test", "demo"],
   },
 ];
